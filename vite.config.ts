bc5d67ae9e3bb899231c9526dbdb0c8a54d19f8c
Chris Hobden <cd@3v.io>
2023-04-04 08:58:15 +0100

    start adding HACS support

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/givtcp-power-flow-card.ts',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      external: [/^lit-element/, /^lit/],
-    },
-  },
+	build: {
+		sourcemap: true,
+		lib: {
+			entry: 'src/givtcp-power-flow-card.ts',
+			formats: ['es'],
+		},
+		rollupOptions: {
+			external: [/^lit-element/, /^lit/],
+		},
+	},
+	optimizeDeps: {
+		disabled: true,
+	}
 })
