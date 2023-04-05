8feea2b29f29aba5c9a58eb6e5e9ca17839cfa0b
Chris Hobden <cd@3v.io>
2023-04-05 10:37:44 +0100

    fixed rendering multiple paths for zero flows

--- src/entity.ts
+++ src/entity.ts
@@ -26,10 +26,10 @@ export class GivTCPPowerFlowCardEntity extends LitElement {
 		this.style.setProperty('--gtpc-color', `var(--gtpc-${this.data.type}-color)`);
 		return html`
 		${svg`<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-		${Object.keys(partTotals).map(key => {
-			const percentage = fullTotal > 0 ? partTotals[key] / fullTotal * 100 : 100;
+		${fullTotal <= 0?svg`<path d="${SVGUtils.getCirclePath(100, 0, 49)}" style="stroke: var(--gtpc-${this.data.type}-color)" />`:Object.keys(partTotals).map((key, idx) => {
+			const percentage = partTotals[key] / fullTotal * 100;
 			offset += fullTotal > 0 ? (fullTotal - partTotals[key]) / fullTotal * 100 : 0;
-			return svg`<path d="${SVGUtils.getCirclePath(percentage, offset, 49)}" style="stroke: var(--gtpc-${key}-color)" />`;
+			return percentage>0?svg`<path d="${SVGUtils.getCirclePath(percentage, offset, 49)}" style="stroke: var(--gtpc-${key}-color)" />`:html``;
 		}
 		)}
 		</svg>`}
