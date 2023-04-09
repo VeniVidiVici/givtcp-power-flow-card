fadd57d16324ca618a53c1e925aa3c7c03bf853e
Chris Hobden <cd@3v.io>
2023-04-09 13:23:01 +0100

    added new config options for custom colours and manually disabling entites

---
+++ src/config-utils.ts
@@ -0,0 +1,69 @@
+import { LovelaceCardConfig } from 'custom-card-helpers';
+import {
+	HIDE_INACTIVE_FLOWS_DEFAULT,
+	COLOUR_ICONS_AND_TEXT_DEFAULT,
+	LINE_GAP_DEFAULT,
+	LINE_WIDTH_DEFAULT,
+	DOT_SIZE_DEFAULT,
+	POWER_MARGIN_DEFAULT,
+	DOT_SPEED_DEFAULT,
+	ENTITY_SIZE_DEFAULT,
+	CIRCLE_SIZE_DEFAULT,
+	COLOUR_TYPE_DEFAULT,
+	GRID_COLOUR_UI_DEFAULT,
+	GRID_COLOUR_RGB_DEFAULT,
+	SOLAR_COLOUR_UI_DEFAULT,
+	SOLAR_COLOUR_RGB_DEFAULT,
+	BATTERY_COLOUR_UI_DEFAULT,
+	BATTERY_COLOUR_RGB_DEFAULT,
+	HOUSE_COLOUR_UI_DEFAULT,
+	HOUSE_COLOUR_RGB_DEFAULT,
+	BATTERY_ENABLED_DEFAULT,
+	SOLAR_ENABLED_DEFAULT,
+} from './const';
+import { EntityLayout } from './types';
+
+export class ConfigUtils {
+	public static getDefaults(config: LovelaceCardConfig): LovelaceCardConfig {
+		const defaults: LovelaceCardConfig = {
+			type: 'custom:givtcp-power-flow-card',
+			hide_inactive_flows: HIDE_INACTIVE_FLOWS_DEFAULT,
+			colour_icons_and_text: COLOUR_ICONS_AND_TEXT_DEFAULT,
+			line_gap: LINE_GAP_DEFAULT,
+			line_width: LINE_WIDTH_DEFAULT,
+			dot_size: DOT_SIZE_DEFAULT,
+			entity_layout: EntityLayout.Cross,
+			power_margin: POWER_MARGIN_DEFAULT,
+			dot_speed: DOT_SPEED_DEFAULT,
+			entity_size: ENTITY_SIZE_DEFAULT,
+			circle_size: CIRCLE_SIZE_DEFAULT,
+			grid_colour_type: COLOUR_TYPE_DEFAULT,
+			solar_colour_type: COLOUR_TYPE_DEFAULT,
+			battery_colour_type: COLOUR_TYPE_DEFAULT,
+			house_colour_type: COLOUR_TYPE_DEFAULT,
+			battery_enabled: BATTERY_ENABLED_DEFAULT,
+			solar_enabled: SOLAR_ENABLED_DEFAULT,
+			grid_colour: config.grid_colour_type === 'rgb' ? GRID_COLOUR_RGB_DEFAULT : GRID_COLOUR_UI_DEFAULT,
+			solar_colour: config.solar_colour_type === 'rgb' ? SOLAR_COLOUR_RGB_DEFAULT : SOLAR_COLOUR_UI_DEFAULT,
+			battery_colour: config.battery_colour_type === 'rgb' ? BATTERY_COLOUR_RGB_DEFAULT : BATTERY_COLOUR_UI_DEFAULT,
+			house_colour: config.house_colour_type === 'rgb' ? HOUSE_COLOUR_RGB_DEFAULT : HOUSE_COLOUR_UI_DEFAULT,
+		};
+		return defaults;
+	}
+	static migrateConfig(config: LovelaceCardConfig, makeCopy: boolean): LovelaceCardConfig {
+		const newConfig = makeCopy ? { ...config } : config;
+		const mappings = {
+			icon_solar: 'solar_icon',
+			icon_battery: 'battery_icon',
+			icon_grid: 'grid_icon',
+			icon_house: 'house_icon',
+		};
+		for (const [oldKey, newKey] of Object.entries(mappings)) {
+			if (newConfig[oldKey]) {
+				newConfig[newKey] = newConfig[oldKey];
+				delete newConfig[oldKey];
+			}
+		}
+		return newConfig;
+	}
+}
--- src/editor.ts
+++ src/editor.ts
@@ -1,25 +1,128 @@
 import { fireEvent, HomeAssistant, LovelaceCardConfig, LovelaceCardEditor, LovelaceConfig } from 'custom-card-helpers';
 import { html, LitElement, TemplateResult } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
-import { ENTITY_SCHEMA, ICON_SCHEMA, LAYOUT_SCHEMA, LAYOUT_TYPE_SCHEMA } from './schemas';
 import {
-	HIDE_INACTIVE_FLOWS_DEFAULT,
-	COLOUR_ICONS_AND_TEXT_DEFAULT,
+	BATTERY_SCHEMA,
+	cardConfigStruct,
+	ENTITY_SCHEMA,
+	GRID_SCHEMA,
+	HOUSE_SCHEMA,
+	LAYOUT_SCHEMA,
+	LAYOUT_TYPE_SCHEMA,
+	SOLAR_SCHEMA,
+} from './schemas';
+import { assert } from 'superstruct';
+import {
 	LINE_WIDTH_DEFAULT,
 	POWER_MARGIN_DEFAULT,
-	LINE_GAP_DEFAULT,
 	DOT_SIZE_DEFAULT,
 	DOT_SPEED_DEFAULT,
 	ENTITY_SIZE_DEFAULT,
+	ENTITIES,
 } from './const';
-import { EntityLayout, UnitOfPower } from './types';
+import { UnitOfPower } from './types';
+import { ConfigUtils } from './config-utils';
 
 @customElement('givtcp-power-flow-card-editor')
 export class GivTCPPowerFlowCardEditor extends LitElement implements LovelaceCardEditor {
 	hass?: HomeAssistant | undefined;
 	lovelace?: LovelaceConfig | undefined;
 	@state() private _config!: LovelaceCardConfig;
+	@state() private _curView?: number;
+	private get _batteries(): string[] {
+		return this.hass
+			? Object.keys(this.hass.states).filter((eid) =>
+					/^sensor\.givtcp_[a-zA-Z]{2}\d{4}[a-zA-Z]\d{3}_battery_serial_number$/g.test(eid)
+			  )
+			: [];
+	}
+	private get _invertors(): string[] {
+		return this.hass
+			? Object.keys(this.hass.states).filter((eid) =>
+					/^sensor\.givtcp_[a-zA-Z]{2}\d{4}[a-zA-Z]\d{3}_invertor_serial_number$/g.test(eid)
+			  )
+			: [];
+	}
+	private get _defaults(): LovelaceCardConfig {
+		return ConfigUtils.getDefaults(this._config);
+	}
+	// private get _invertorsAndBatteries(): string[] {
+	// 	return this.hass ? Object.keys(this.hass.states).filter((eid) =>
+	// 	/^sensor\.givtcp_[a-zA-Z]{2}\d{4}[a-zA-Z]\d{3}_(invertor|battery)_serial_number$/g.test(eid)
+	// ) : [];
+	// }
+	private get _schema(): object[] {
+		switch (this._curView) {
+			case 0:
+				return [
+					{ name: 'name', label: 'Name', selector: { text: {} } },
+					...ENTITY_SCHEMA(this._invertors, this._batteries),
+					{
+						type: 'grid',
+						name: '',
+						schema: [
+							{
+								name: 'dot_size',
+								label: 'Dot Size',
+								default: DOT_SIZE_DEFAULT,
+								selector: { number: { mode: 'slider', min: 1, max: 10 } },
+							},
+							{
+								name: 'dot_speed',
+								label: 'Dot Speed',
+								default: DOT_SPEED_DEFAULT,
+								selector: { number: { mode: 'slider', min: 1, max: 10 } },
+							},
+							{
+								name: 'line_width',
+								label: 'Line Width',
+								default: LINE_WIDTH_DEFAULT,
+								selector: { number: { mode: 'slider', min: 1, max: 10 } },
+							},
+							{
+								name: 'entity_size',
+								label: 'Entity Size',
+								default: ENTITY_SIZE_DEFAULT,
+								selector: { number: { mode: 'slider', min: 3, max: 7 } },
+							},
+						],
+					},
+					{
+						type: 'grid',
+						name: '',
+						schema: [
+							{
+								name: 'power_margin',
+								label: 'Power Threshold',
+								default: POWER_MARGIN_DEFAULT,
+								selector: { number: { mode: 'box', unit_of_measurement: UnitOfPower.WATT } },
+							},
+							{ name: 'hide_inactive_flows', label: 'Hide Inactive Flows', selector: { boolean: {} } },
+							{ name: 'colour_icons_and_text', label: 'Colour Icons and Text', selector: { boolean: {} } },
+						],
+					},
+				];
+			case 1:
+				return [...LAYOUT_SCHEMA, ...LAYOUT_TYPE_SCHEMA(this._config?.entity_layout)];
+			case 2:
+				return [...GRID_SCHEMA(this._config?.grid_colour_type)];
+			case 3:
+				return [...SOLAR_SCHEMA(this._config?.solar_colour_type)];
+			case 4:
+				return [...BATTERY_SCHEMA(this._config?.battery_colour_type)];
+			case 5:
+				return [...HOUSE_SCHEMA(this._config?.house_colour_type)];
+			default:
+				return [];
+		}
+	}
+	constructor() {
+		super();
+		this._curView = 0;
+	}
 	public setConfig(config: LovelaceCardConfig): void {
+		config = ConfigUtils.migrateConfig(config, false);
+		assert(config, cardConfigStruct);
 		this._config = config;
 	}
 	protected render(): TemplateResult {
@@ -27,112 +130,49 @@ export class GivTCPPowerFlowCardEditor extends LitElement implements LovelaceCar
 			return html``;
 		}
 		const data = {
-			hide_inactive_flows: HIDE_INACTIVE_FLOWS_DEFAULT,
-			colour_icons_and_text: COLOUR_ICONS_AND_TEXT_DEFAULT,
-			line_gap: LINE_GAP_DEFAULT,
-			line_width: LINE_WIDTH_DEFAULT,
-			dot_size: DOT_SIZE_DEFAULT,
-			entity_layout: EntityLayout.Cross,
-			power_margin: POWER_MARGIN_DEFAULT,
-			dot_speed: DOT_SPEED_DEFAULT,
-			entity_size: ENTITY_SIZE_DEFAULT,
+			...this._defaults,
 			...this._config,
 		};
-		// const regex = /^sensor\.givtcp_[a-zA-Z]{2}\d{4}[a-zA-Z]\d{3}_(invertor|battery)_serial_number$/g;
-		const invertors = Object.keys(this.hass.states).filter((eid) =>
-			/^sensor\.givtcp_[a-zA-Z]{2}\d{4}[a-zA-Z]\d{3}_invertor_serial_number$/g.test(eid)
-		);
-		const batteries = Object.keys(this.hass.states).filter((eid) =>
-			/^sensor\.givtcp_[a-zA-Z]{2}\d{4}[a-zA-Z]\d{3}_battery_serial_number$/g.test(eid)
-		);
-		const schema = [
-			{ name: 'name', selector: { text: {} } },
-			...ENTITY_SCHEMA(invertors, batteries),
-			...ICON_SCHEMA,
-			{
-				name: 'power_margin',
-				default: POWER_MARGIN_DEFAULT,
-				selector: { number: { mode: 'box', unit_of_measurement: UnitOfPower.WATT } },
-			},
-			{
-				type: 'grid',
-				name: '',
-				schema: [
-					{
-						name: 'dot_size',
-						default: DOT_SIZE_DEFAULT,
-						selector: { number: { mode: 'slider', min: 1, max: 10 } },
-					},
-					{
-						name: 'line_width',
-						default: LINE_WIDTH_DEFAULT,
-						selector: { number: { mode: 'slider', min: 1, max: 10 } },
-					},
-					{
-						name: 'dot_speed',
-						default: DOT_SPEED_DEFAULT,
-						selector: { number: { mode: 'slider', min: 1, max: 10 } },
-					},
-					{
-						name: 'entity_size',
-						default: ENTITY_SIZE_DEFAULT,
-						selector: { number: { mode: 'slider', min: 3, max: 7 } },
-					},
-				],
-			},
-			...LAYOUT_SCHEMA,
-			...LAYOUT_TYPE_SCHEMA(this._config?.entity_layout),
-			{
-				type: 'grid',
-				name: '',
-				schema: [
-					{ name: 'hide_inactive_flows', selector: { boolean: {} } },
-					{ name: 'colour_icons_and_text', selector: { boolean: {} } },
-				],
-			},
-		];
+		ENTITIES.forEach((key) => {
+			if (
+				(data[key + '_colour_type'] !== 'ui' && typeof data[key] === 'string') ||
+				(data[key + '_colour_type'] === 'ui' && typeof data[key] === 'object')
+			) {
+				data[key] = this._defaults[key];
+			}
+		});
 		return html`
+			<ha-tabs scrollable .selected=${this._curView} @iron-activate=${this._handleTabChanged}>
+				<paper-tab>General</paper-tab>
+				<paper-tab>Layout</paper-tab>
+				<paper-tab>Grid</paper-tab>
+				<paper-tab>Solar</paper-tab>
+				<paper-tab>Battery</paper-tab>
+				<paper-tab>House</paper-tab>
+				<paper-tab>Extra</paper-tab>
+			</ha-tabs>
 			<ha-form
 				.hass=${this.hass}
 				.data=${data}
-				.schema=${schema}
+				.schema=${this._schema}
 				.computeLabel=${this._computeLabelCallback}
 				@value-changed=${this._valueChanged}
 			></ha-form>
 		`;
 	}
-	private _computeLabelCallback = (schema: { name: string }) => {
+	private _handleTabChanged(ev: CustomEvent): void {
+		const tab = ev.detail.selected as number;
+		this._curView = tab;
+	}
+	private _computeLabelCallback = (schema: { name: string; label?: string }) => {
+		if (schema.label) return schema.label;
 		switch (schema.name) {
 			case 'invertor':
 				return 'Invertor';
 			case 'battery':
 				return 'Battery';
-			case 'dot_size':
-				return 'Dot Size';
-			case 'dot_speed':
-				return 'Dot Speed';
-			case 'icon_battery':
-				return 'Battery Icon';
-			case 'icon_grid':
-				return 'Grid Icon';
-			case 'icon_house':
-				return 'House Icon';
-			case 'icon_solar':
-				return 'Solar Icon';
 			case 'entity_layout':
 				return 'Layout';
-			case 'entity_size':
-				return 'Entity Size';
-			case 'hide_inactive_flows':
-				return 'Hide Inactive Flows';
-			case 'colour_icons_and_text':
-				return 'Colour Icons and Text';
-			case 'power_margin':
-				return 'Power Margin';
-			case 'line_width':
-				return 'Line Width';
-			case 'line_gap':
-				return 'Line Gap';
 			case 'circle_size':
 				return 'Circle Size';
 			case 'centre_entity':
@@ -142,7 +182,8 @@ export class GivTCPPowerFlowCardEditor extends LitElement implements LovelaceCar
 		}
 	};
 	private _valueChanged(ev: CustomEvent): void {
-		fireEvent(this, 'config-changed', { config: ev.detail.value });
+		const config = ev.detail.value;
+		fireEvent(this, 'config-changed', { config });
 	}
 }
 declare global {
